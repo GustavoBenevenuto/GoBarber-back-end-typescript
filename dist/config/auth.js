39c42fe056e64fb63c173c0a2b2fa99e0190a662
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2021-11-27 18:32:14 -0300

    Ajustes

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        scret: '9b7cd07d417c11f15670c808538f1b36',
+        expiresIn: '1d',
+    }
+};
---
+++ dist/errors/AppError.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Error = /** @class */ (function () {
+    function Error(message, statusCode) {
+        if (statusCode === void 0) { statusCode = 400; }
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+    return Error;
+}());
+exports.default = Error;
+//readonly impede se setar o atributo diretamente
